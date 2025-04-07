719b38c51335c5c12e4a24496af619906e764837
pagiannis <pagiannis04@gmail.com>
2025-04-07 19:09:17 +0300

    Improve layout on ErrorPage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -6,7 +6,7 @@ const ErrorPage = () => {
   return (
     <>
       <h1 className="text-4xl font-bold text-center mt-10">Oops</h1>
-      <p>
+      <p className="text-center mt-4">
         {isRouteErrorResponse(error)
           ? "This page does not exist."
           : "An unexpected error occured."}{" "}
