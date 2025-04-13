d0a5eca19d9c4df3e4624d9af192af0c4a386908
pagiannis <pagiannis04@gmail.com>
2025-04-13 13:28:12 +0300

    Set up env variables

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: import.meta.env.VITE_API_CLIENT_BASE_URL, // Base URL for the API
     withCredentials: true,    // Ensures cookies are sent
 })
