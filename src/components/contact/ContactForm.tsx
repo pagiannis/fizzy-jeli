f30aff68e08e003cce22725fd0cf0bea498dd5ff
pagiannis <pagiannis04@gmail.com>
2025-04-16 22:01:05 +0300

    UseRef for ContactForm submition approach

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,6 +1,39 @@
+import { useRef } from "react";
+
 const ContactForm = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const subjectRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
+  const formData = {
+    name: "",
+    subject: "",
+    email: "",
+    message: "",
+  };
+
+  const hangleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (nameRef.current !== null) {
+      formData.name = nameRef.current.value;
+    }
+    if (subjectRef.current !== null) {
+      formData.subject = subjectRef.current.value;
+    }
+    if (emailRef.current !== null) {
+      formData.email = emailRef.current.value;
+    }
+    if (messageRef.current !== null) {
+      formData.message = messageRef.current.value;
+    }
+    console.log(formData);
+
+    // Reset form fields
+    
+  };
+
   return (
-    <div className="h-[calc(100vh-8rem)] flex items-center justify-center p-4 font-serif">
+    <div className="h-[calc(100vh-4rem)] flex items-center justify-center p-4 font-serif">
       <div className="w-full max-w-2xl">
         <div className="bg-purple-200 rounded-2xl shadow-md overflow-hidden select-none drag-none p-10">
           {/* Form Header */}
@@ -10,15 +43,17 @@ const ContactForm = () => {
             </h2>
           </div>
 
-          <form className="space-y-5">
+          <form className="space-y-5" onSubmit={hangleSubmit}>
             {/* Name Input */}
             <div className="space-y-1">
               <label className="block text-md font-medium text-pink-500 ml-4">
                 Name
               </label>
               <input
+                ref={nameRef}
+                id="name"
                 type="text"
-                className="w-full px-4 py-2 border border-pink-400 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all"
+                className="text-pink-500 w-full px-4 py-2 border border-pink-400 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all"
                 placeholder="Name"
               />
             </div>
@@ -29,8 +64,10 @@ const ContactForm = () => {
                 Subject
               </label>
               <input
+                ref={subjectRef}
+                id="subject"
                 type="text"
-                className="w-full px-4 py-2 border border-pink-400 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all"
+                className="text-pink-500 w-full px-4 py-2 border border-pink-400 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all"
                 placeholder="Subject"
               />
             </div>
@@ -41,8 +78,10 @@ const ContactForm = () => {
                 Email
               </label>
               <input
+                ref={emailRef}
+                id="email"
                 type="email"
-                className="w-full px-4 py-2 border border-pink-400 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all"
+                className="text-pink-500 w-full px-4 py-2 border border-pink-400 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all"
                 placeholder="Email"
               />
             </div>
@@ -53,8 +92,10 @@ const ContactForm = () => {
                 Message
               </label>
               <textarea
+                ref={messageRef}
+                id="message"
                 rows={4}
-                className="w-full px-4 py-2 border border-pink-400 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all"
+                className="text-pink-500 w-full px-4 py-2 border border-pink-400 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all"
                 placeholder="Message"
               ></textarea>
             </div>
