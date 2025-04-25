b43b6d8c0d8b7284cad87c89343f2f35a0ed60c4
pagiannis <pagiannis04@gmail.com>
2025-04-25 21:42:41 +0300

    About paragraph added

--- src/components/about/AboutDiv.tsx
+++ src/components/about/AboutDiv.tsx
@@ -2,8 +2,13 @@ import { CardContainer } from "../ui/CardContainer";
 
 const AboutDiv = () => {
   return (
-    <CardContainer title="ABOUT" width={4}>
-      <p>About us bla bla blab la bla bla bla bla bla bla</p>
+    <CardContainer title="ABOUT" width={4} className="pt-20">
+      <p className="text-center text-pink-500 text-lg leading-relaxed">
+        At Fizy-Jeli, we craft effervescent delights that spark joy in every
+        bite and sip. Our minimalist approach celebrates pure flavors—no
+        artificial additives, just playful bubbles and clean sweetness. Designed
+        for those who appreciate subtle sophistication in life's little fizzes.
+      </p>
     </CardContainer>
   );
 };
