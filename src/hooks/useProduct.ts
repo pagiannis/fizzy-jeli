7e19cb859bb2ee5aad29d5bcb64863a4bd0459ee
pagiannis <pagiannis04@gmail.com>
2025-04-27 20:41:29 +0300

    create useProduct hook

---
+++ src/hooks/useProduct.ts
@@ -0,0 +1,34 @@
+import { useParams } from "react-router-dom";
+import apiClient from "../services/api-client";
+import { useQuery } from "@tanstack/react-query";
+
+interface Product {
+  _id: string;
+  name: string;
+  price: number;
+  image: string;
+  secondaryImage?: string;
+}
+
+const useProduct = () => {
+  const { productId } = useParams();
+
+  const fetchProduct = async () => {
+    try {
+      const response = await apiClient.get<Product>(`/products/${productId}`);
+      return response.data;
+    } catch (err) {
+      throw new Error("Failed to fetch product");
+    }
+  };
+
+  const { data: product } = useQuery<Product, Error>({
+    queryKey: ["products", productId],
+    queryFn: fetchProduct,
+    staleTime: 1 * 60 * 1000,
+  });
+
+  return { product };
+}
+
+export default useProduct;
--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,29 +1,7 @@
-import { useParams } from "react-router-dom";
-import apiClient from "../services/api-client";
-import { Product } from "../hooks/useProducts";
-import { useQuery } from "@tanstack/react-query";
+import useProduct from "../hooks/useProduct";
 
 const ProductDetailPage = () => {
-  const { productId } = useParams();
-
-  if (!productId) {
-    return <div>Error: Product ID not found in URL</div>;
-  }
-
-  const fetchProduct = async () => {
-    try {
-      const response = await apiClient.get<Product>(`/products/${productId}`);
-      return response.data;
-    } catch (err) {
-      throw new Error("Failed to fetch product");
-    }
-  };
-
-  const { data: product } = useQuery<Product, Error>({
-    queryKey: ["products", productId],
-    queryFn: fetchProduct,
-    staleTime: 1 * 60 * 1000,
-  });
+  const { product } = useProduct();
 
   return <div className="flex-1">{product?.name}</div>;
 };
