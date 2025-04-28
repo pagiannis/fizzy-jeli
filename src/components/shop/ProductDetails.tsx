6ee4348f8bbf37547590f4217af49c0c8a4d6765
pagiannis <pagiannis04@gmail.com>
2025-04-28 19:44:57 +0300

    ProductDetails ui 4

--- src/components/shop/ProductDetails.tsx
+++ src/components/shop/ProductDetails.tsx
@@ -8,7 +8,7 @@ const ProductDetails = () => {
   return (
     <CardContainer width={7} className="p-0 m-0">
       {/* Add min-h-[500px] or similar to establish a height context */}
-      <div className="flex flex-col lg:flex-row gap-15 sm:p-6 min-h-[500px]">
+      <div className="flex flex-col lg:flex-row lg:gap-15 sm:p-6 min-h-[500px]">
         {/* Left column - now properly centered */}
         <div className="lg:order-1 lg:w-1/2 flex items-center justify-center">
           <div className="font-chewy text-pink-400 text-center">
@@ -25,13 +25,13 @@ const ProductDetails = () => {
         </div>
 
         {/* Combined middle + right column */}
-        <div className="lg:order-2 lg:order-1 lg:w-1/2  rounded-lg flex flex-col lg:flex-row p-10">
+        <div className="lg:order-2 lg:order-1 lg:w-1/2  rounded-lg flex flex-col lg:flex-row p-3 lg:p-10">
           {/* Right (description + button) */}
           <div className="flex flex-col justify-center items-center">
             <p className="order-2 space-y-2 font-chewy text-bold text-white text-center text-lg md:text-2xl bg-[linear-gradient(160deg,rgba(238,130,207,0.74)_0%,rgb(177,124,247)_100%)] p-8 rounded-lg shadow-md">
               {product?.description}
             </p>
-            <Button className="order-1 lg:order-2 mt-10 font-chewy text-lg md:text-2xl">
+            <Button className="order-1 lg:order-2 mb-15 md:mt-10 font-chewy text-lg md:text-2xl cursor-pointer">
               Add to Cart
             </Button>
           </div>
