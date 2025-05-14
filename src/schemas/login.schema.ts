45ab6fd8c72ed4199b7be3739f1e0241a9068db8
pagiannis <pagiannis04@gmail.com>
2025-05-14 15:41:49 +0300

    Fix login error message for empty password 2

--- src/schemas/login.schema.ts
+++ src/schemas/login.schema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const LoginSchema = z.object({
   email: z
     .string()
+    .min(1, "Email is required")
     .email("Invalid email address"),
     
   password: z
