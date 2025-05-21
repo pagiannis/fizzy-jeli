476a52f86c287aa2bb062a83c98bb900fae946c4
pagiannis <pagiannis04@gmail.com>
2025-05-21 12:35:27 +0300

    Remove auto-login on register

--- src/hooks/useRegister.ts
+++ src/hooks/useRegister.ts
@@ -3,7 +3,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { RegisterSchema, RegisterFormData} from "../schemas/register.schema";
 import { useMutation } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
-import { useAuth } from "../contexts/AuthContext";
 
 interface UseRegisterOptions {
   onSuccess?: () => void;  // Make it optional with ?
@@ -14,25 +13,13 @@ export const useRegister = ({ onSuccess }: UseRegisterOptions = {}) => {
     resolver: zodResolver(RegisterSchema),
   });
 
-  const { login } = useAuth();
-
   const mutation = useMutation({
     mutationFn: async (data: RegisterFormData) => {
       const { confirmPassword, ...registrationData } = data;
 
       const res =  await apiClient.post("/users", registrationData);
-      console.log("Registration response:", res);
 
-      const token = res.headers['x-auth-token'];
-      const user = res.data;
-      
-      if (!token || !user) {
-        throw new Error("Registration succeeded but token or user data is missing.");
-      }
-
-      login(token, user);
-
-      return user;
+      return res.data;
     },
     onSuccess: () => {
       form.reset();
