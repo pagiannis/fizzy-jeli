5dcfc1f6f3eb38532ead55f9ef49a71778c99b84
pagiannis <pagiannis04@gmail.com>
2025-07-07 23:03:06 +0300

    React devtools (ONLY DEV MODE!!!)

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Toaster } from "react-hot-toast";
 
@@ -20,6 +21,7 @@ createRoot(document.getElementById("root")!).render(
             duration: 4000,
           }}
         />
+        <ReactQueryDevtools />
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
