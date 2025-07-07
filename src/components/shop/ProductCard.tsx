3f77a273d2eeaf7cfecc1346c608eab6bde8c1a6
pagiannis <pagiannis04@gmail.com>
2025-07-07 22:29:55 +0300

    typo

--- src/components/shop/ProductCard.tsx
+++ src/components/shop/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Product } from "../../hooks/useProducts";
+import { Product } from "../../hooks/useProduct";
 import { useState } from "react";
 
 interface Props {
